39351d3328cb8ef9f691f0c1bb3960da563745e2
Turner <1266213561121004@mil>
2021-10-26 09:28:51 -0400

    auto commit

--- call_script.R
+++ call_script.R
@@ -2,10 +2,12 @@ library(tidyverse)
 
 
 leagueID <- list(89417258)
+# leagueID <- list(35354777)
 # leagueID <- list(847888,35354777,89417258,206814)
 # names <- list("jim","headshed","OA","Twitter_Guy")
 # is_dusty = TRUE
 names <- list("OA")
+# names <- list("headshed")
 per_id <- 6
 
 run_reports <- function(leagueID, per_id = per_id, names) {
